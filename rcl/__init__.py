4f2f67614b7b220aeed0bab47be63108f687031c
lasickim <39771856+lasickim@users.noreply.github.com>
2026-08-14 15:42:57 +0900

    Expose Behavior Intent APIs and reference adapter

--- rcl/__init__.py
+++ rcl/__init__.py
@@ -1,4 +1,9 @@
-from .adapter import BehaviorMigrationResult, RCLAdapter
+from .adapter import (
+    BehaviorMigrationResult,
+    ExpressionMigrationResult,
+    IntentMigrationResult,
+    RCLAdapter,
+)
 from .capabilities import (
     CapabilityClassification,
     CapabilityValidationError,
@@ -36,6 +41,14 @@ from .habit_policy import (
     load_default_habit_promotion_policy,
 )
 from .history import HABIT_LIFECYCLES, validate_behavior_habit_metadata
+from .intent import (
+    INTENT_VOCABULARY_VERSION,
+    get_intent_goal,
+    load_intent_vocabulary,
+    registered_intent_goals,
+    validate_behavior_intent_metadata,
+)
+from .intent_reference_adapter import IntentReferenceAdapter
 from .migration import migrate_profile
 from .profile import RCLProfile, RCLValidationError, RCL_VERSION
 from .profile_diff import (
@@ -74,12 +87,16 @@ __all__ = [
     "DEFAULT_MIN_TRIALS",
     "EVALUATION_METHOD",
     "EVALUATION_VERSION",
+    "ExpressionMigrationResult",
     "HABIT_APPROVAL_APPLY_METHOD",
     "HABIT_APPROVAL_PATCH_METHOD",
     "HABIT_APPROVAL_VERSION",
     "HABIT_LIFECYCLES",
     "HABIT_PROMOTION_METHOD",
     "HABIT_PROMOTION_VERSION",
+    "INTENT_VOCABULARY_VERSION",
+    "IntentMigrationResult",
+    "IntentReferenceAdapter",
     "PROFILE_DIFF_METHOD",
     "PROFILE_DIFF_VERSION",
     "RCLAdapter",
@@ -101,11 +118,14 @@ __all__ = [
     "evaluate_observed_continuity",
     "evaluate_repeated_sessions",
     "get_capability",
+    "get_intent_goal",
     "load_capability_registry",
     "load_default_habit_promotion_policy",
+    "load_intent_vocabulary",
     "migrate_profile",
     "preview_habit_approval",
     "registered_capabilities",
+    "registered_intent_goals",
     "reserved_namespaces",
     "run_adapter_conformance",
     "sample_mean",
@@ -113,6 +133,7 @@ __all__ = [
     "t95_critical",
     "validate_behavior_evaluation_metadata",
     "validate_behavior_habit_metadata",
+    "validate_behavior_intent_metadata",
     "validate_capability_id",
     "validate_capability_set",
     "wasserstein_1d",
